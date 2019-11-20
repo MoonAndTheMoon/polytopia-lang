bb9a0a7824e7cdfde1892562ab094f15fa71e863
MoonAndTheMoon <35038532+MoonAndTheMoon@users.noreply.github.com>
2019-11-20 16:47:40 +0100

    update

--- js_check.js
+++ js_check.js
@@ -1,31 +1,42 @@
 <script>
-let url_EN = "https://midjiwan.com/lang/en_US.json";
-let url_PT = "https://midjiwan.com/lang/pt_BR.json";
-let url_NO = "https://raw.githubusercontent.com/MoonAndTheMoon/polytopia-lang/master/nb_NO.json";
+let prefix = "https://raw.githubusercontent.com/MoonAndTheMoon/polytopia-lang/master/";
+let url_EN = prefix + "en_US.json";
+let url_PT = prefix + "pt_BR.json";
+let url_NO = prefix + "nb_NO.json";
 
 var getJSON = function(url, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
 	xhr.responseType = 'json';
 	xhr.onload = function() {
-	var status = xhr.status;
-	if (status === 200) {
-		callback(null, xhr.response);
-	} else {
-		callback(status, xhr.response);
-	}
+		var status = xhr.status;
+		if (status === 200) {
+			callback(xhr.response);
+		}
 	};
 	xhr.send();
 };
 
-getJSON(url_NO,
-	function(err, data) {
-		if (err !== null) {
-			document.write("Error: ", err);
-		} else {
-			document.write(data.language);
-		}
+let EN, PT;
+getJSON(url_EN,
+	function(data) {
+		EN = data;
+		compare();
 });
+
+getJSON(url_PT,
+	function(data) {
+		PT = data;
+		compare();
+});
+
+let loadedCount = 0;
+var compare = function() {
+	loadedCount += 1;
+	if (loadedCount == 2) {
+		document.write(EN.language, "<br>", PT.language);
+	}
+};
 
 //document.write();
 </script>
